cd1455a351b8eee9f96859fac6fcfc756c32ace3
ashanshashiranga <ashansashiranga@gmail.com>
2019-09-01 13:36:26 +0530

    everything done

--- js/login.js
+++ js/login.js
@@ -1,13 +1,11 @@
-function validateEmail(){
-    var email=document.getElementById("username").value;
-    if(/^\S+@\S+$/.test(email)){
-       document.getElementById("userError").innerHTML="";
+function validateEmail() {
+    var email = document.getElementById("username").value;
+    if (/^\S+@\S+$/.test(email)) {
+        document.getElementById("userError").innerHTML = "";
         return true;
-    }
-    else{
-        document.getElementById("userError").innerHTML="Please enter a valid email address. example@email.com";
+    } else {
+        document.getElementById("userError").innerHTML = "Please enter a valid email address. example@email.com";
         return false;
     }
 
 }
-
