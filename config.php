3b44d8d261581b5d21336b7036165edf07b64a75
WhizSid <whizsid@aol.com>
2019-10-04 00:48:40 +0530

    Changed configurations

--- config.php
+++ config.php
@@ -6,8 +6,8 @@ $config = [
         "host"=>"127.0.0.1",
         "port"=>"3306",
         "user"=>"root",
-        "password"=>"warurami",
-        "database"=>"meal_cart_iwt"
+        "password"=>"",
+        "database"=>"meal_cart"
     ],
 
     // Application configurations
