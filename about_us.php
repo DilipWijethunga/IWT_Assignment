ca93d9d3aa177e6a0af4e53df626f34838e7abb2
Isharud <54754925+Isharud@users.noreply.github.com>
2019-10-06 14:27:40 +0530

    aboutus

--- about_us.php
+++ about_us.php
@@ -24,6 +24,28 @@ require_once "init.php";
 			 </b></b></div><b><b>
                     
              <div class="content">
+                 <div class = "history">
+                     <h2>Our History</h2><br>
+
+                         <p>The Chinese Dragon Café (Pvt) Ltd is Sri Lanka’s oldest and most respected restaurant chain. Started in 1942 by a family of Chinese immigrants. Chinese Dragon Café (Pvt) Ltd has pioneered Chinese cuisine in Sri Lanka, offering authentic food refined specifically to suit the local palate. Authentic dishes such as our Foo Young Hai and our caramel pudding are still prepared just as they were when we first opened over half a century ago. Popular innovations have also become a trademark - our Hot Buttered Cuttlefish, Crab Claws, Hot and Spicy Seafood Soup (Tom Yum), Seafood Fried Rice, Chilli Chicken, Chilli Paste and devilled dishes have become synonymous with Chinese food in Sri Lanka – often imitated but never replicated.</p><br><br>
+
+                         <p> For a taste of the food that you have come to know and love, visit one of our branches or use our online ordering page to experience Sri Lanka’s premier delivery service and enjoy our dishes in the comfort of your own home.</p><br><br>
+
+                         <p>The ambience of Chinese Dragon Café plays a primary role in attracting families, groups of friends, locals and foreigners to patronize its restaurants’ eager to sooth their taste buds with exceptional service provided by experienced and friendly staff.</p><br><br>
+
+                         <p>To our loyal customers we thank you for your continued patronage and are thrilled that you are a part of the Chinese Dragon family.</p><br><br>
+
+                         <h2>Our Vision</h2>
+
+                         <p>"To become the preferred dining experience where we operate"<p><br><br>
+
+                             <h2>Our Mission</h2><br>
+
+                             <p>"To expand the dining experience beyond traditional boundaries while providing   prompt, professional & friendly service to consistently provide a reasonably priced, Well prepared meal using quality ingredients in our recipes and meeting the highest standards of all our stakeholders".</p><br>
+                             
+                         </div>
+
+  
                  <div class="content-col">
                      <h2>Browse</h2>
                      <p>Online Meal Cart has hundreds of restaurants to choose from. When you open the app, you can scroll through the feed for inspiration or search for a particular restaurant or cuisine. When you find something you like, tap to add it to your cart.</p>
